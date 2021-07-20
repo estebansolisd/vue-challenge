aa654792df190b5d4121cd6fde7bb1dc78b2f138
Esteban Solis <estebansolisd@gmail.com>
2021-07-19 19:50:30 -0600

    Adding materialize and creating mock data

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+declare var M: any;
+// Adding materialize
+import 'materialize-css/dist/css/materialize.css'
+import 'materialize-css/dist/js/materialize.js'
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
+  mounted(){
+    M.AutoInit()
+  }
 }).$mount("#app");
